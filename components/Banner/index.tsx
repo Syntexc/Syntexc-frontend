cf20f809dac3b51f8a6ba9664bb1070af9d8e211
sahilsimply <102518542+sahilsimply@users.noreply.github.com>
2025-03-30 09:20:36 -0400

    updated code

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -9,7 +9,7 @@ const Banner = () => {
             <div className={Style.banner_content}>
                 
                 {/* <div className={Style.content_image}> */}
-                <div className={Style.middlestyle}>
+                {/* <div className={Style.middlestyle}>
                     <h1>
                     SYNEXC
                 </h1>
@@ -17,34 +17,33 @@ const Banner = () => {
                 
                 <p>Synergy That Drives Success</p>
                 <h2>WHAT IF YOUR CRM DID <br /><span>MORE THAN JUST</span><br /> MANAGE CUSTOMERS?</h2>
-                    {/* <Image src={"/Frame 21.png"} width={682} height={412} alt="banner" /> */}
-                {/* </div> */}
+                    
                 <button>Get Started.<span>IT'S FOR FREE!</span></button>
-                </div>
-                <Image src={"/Asset 1 1.png"} width={171} height={203} alt="banner"  className={Style.image_content}/>
+                </div> */}
+                {/* <Image src={"/Asset 1 1.png"} width={171} height={203} alt="banner"  className={Style.image_content}/> */}
 
             </div>
             <div className={Style.banner_image}>
                 {/* Add your image here */}
                 <div className={Style.bottomstyle}>
-                <Image src={"/Frame 427319105.png"} width={88} height={88} alt="banner"  className={Style.image1}/>
+                <Image src={"/Frame 427319105.png"} width={120} height={120} alt="banner"  className={Style.image1}/>
                 <Image src={"/Group (2).png"} width={271} height={163} alt="banner"  className={Style.image2}/>
                 </div>
                 <div className={Style.bottomstyle1}>
                 <div className={Style.bottomstyle2}>
                     
-                <Image src={"/pic1.png"} width={75} height={75} alt="banner"  className={Style.image6} />
+                <Image src={"/pic1.png"} width={120} height={120} alt="banner"  className={Style.image6} />
                 {/* <div style={{display: "flex", flexDirection: "column",}}> */}
                 {/* <div> */}
                 
 
-                <Image src ={"/Frame 427319127 (1).png"} width={88} height={88} alt="banner" className={Style.image4}  />
-                <Image src={"/Group (4).png"} width={63} height={76} alt="banner"  className={Style.image7} />
+                <Image src ={"/Frame 427319127 (1).png"} width={120} height={120} alt="banner" className={Style.image4}  />
+                <Image src={"/Group (4).png"} width={120} height={120} alt="banner"  className={Style.image7} />
           
 
                 {/* </div> */}
                 </div>
-                <Image src={"/Frame 427319075 (1).png"} width={332} height={332} alt="banner"  className={Style.image5} />
+                <Image src={"/Frame 427319075 (1).png"} width={450} height={450} alt="banner"  className={Style.image5} />
                
                 {/* </div> */}
                 {/* <div style={{display: "flex", flexDirection: "column",}}> */}
