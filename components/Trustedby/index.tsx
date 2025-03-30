889730c78c55e9070558c0f860265f82ad0bd3d9
sahilsimply <102518542+sahilsimply@users.noreply.github.com>
2025-03-30 09:03:53 -0400

    updated code

--- components/Trustedby/index.tsx
+++ components/Trustedby/index.tsx
@@ -1,6 +1,26 @@
+"use client"
 import RatingCard from "../RatingCard";
 import Styles from "./trustedby.module.scss"
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import { useEffect, useRef, useState } from "react";
+import Image from "next/image";
 const Trustedby = () =>{
+
+    const [slides, setSlides] = useState(3);
+
+    useEffect(() => {
+      const updateSlides = () => {
+        setSlides(window.innerWidth < 500 ? 1.1 : 3);
+      };
+      updateSlides();
+      window.addEventListener('resize', updateSlides);
+      return () => window.removeEventListener('resize', updateSlides);
+    }, []);
+
+    const swiperRef = useRef<Swiper | null>(null);
     return(
         <>
         <section className={Styles.trustedbybox}>
@@ -23,32 +43,58 @@ const Trustedby = () =>{
 
 
 <div className={Styles.ratingcard2}>
-    <RatingCard 
-    image={"/user1.png"}
-    username="Alex R."
-    role="Director of Sales  Tech Solutions Inc."
-    desc="Sales Cloud completely changed our sales process, thanks to Synexc. We struggled with manual lead tracking and disorganiz"
-    />
-    <RatingCard 
-    image={"/user1.png"}
-    username="Alex R."
-    role="Director of Sales  Tech Solutions Inc."
-    desc="With Service Cloud and automation from Synexc, our customer support team is thriving. Before, tickets were scattered across different channels.."
-    />
-    <RatingCard 
-    image={"/user1.png"}
-    username="Alex R."
-    role="Director of Sales  Tech Solutions Inc."
-    desc="We wanted to launch our app on the Salesforce AppExchange, but the security review process felt like a mountain to climb. Synexc handled .."
-    />
+<div className="testimonial-container">
+      <Swiper
+        modules={[Autoplay, Navigation]}
+        loop={true}
+        pagination={{ clickable: true }}
+        navigation={{
+          prevEl: ".custom-prev",
+          nextEl: ".custom-next",
+        }}
+        spaceBetween={30}
+        slidesPerView={3}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+        }}
+        onSwiper={(swiper) => (swiperRef.current = swiper)}
+        className="testimonialslider"
+      >
+        {ratingCards?.length > 0 ? (
+          ratingCards.map((card, index) => (
+            <SwiperSlide key={index}>
+              <RatingCard
+                image={card.image}
+                username={card.username}
+                role={card.role}
+                desc={card.desc}
+              /> 
+            </SwiperSlide>
+          ))
+        ) : (
+          <p>Loading testimonials...</p>
+        )}
+      </Swiper>
+
+      
+     
+    </div>
+    
 </div>
+
 
 
 
 
                 </div>
 
+                <div className={Styles.arrowButton}>
 
+<div className={`${Styles.arrowButtonLeft} swiper-button-prev`} onClick={() => swiperRef.current?.slidePrev()}><Image src="/svg-icons/arrow-left-s-line.svg" width={25}  height={25} alt="Logo" /></div>
+<div className={`${Styles.arrowButtonRight} swiper-button-next `}  onClick={() => swiperRef.current?.slideNext()}><Image src="/svg-icons/arrow-right-s-line.svg" width={25}  height={25} alt="Logo" /></div>
+</div>
                 <div className={Styles.row3}>
     <a href="">Join our community </a>
     </div>
@@ -59,3 +105,43 @@ const Trustedby = () =>{
 }
 
 export default Trustedby ;
+
+
+const ratingCards = [
+    {
+        image: "/user1.png",
+        username: "Alex R.",
+        role: "Director of Sales  Tech Solutions Inc.",
+        desc: "Sales Cloud completely changed our sales process, thanks to Synexc. We struggled with manual lead tracking and disorganiz",
+    },
+    {
+        image: "/user1.png",
+        username: "Alex R.",
+        role: "Director of Sales  Tech Solutions Inc.",
+        desc: "With Service Cloud and automation from Synexc, our customer support team is thriving. Before, tickets were scattered across different channels..",
+    },
+    {
+        image: "/user1.png",
+        username: "Alex R.",
+        role: "Director of Sales  Tech Solutions Inc.",
+        desc: "We wanted to launch our app on the Salesforce AppExchange, but the security review process felt like a mountain to climb. Synexc handled ..",
+    },
+    {
+        image: "/user1.png",
+        username: "Alex R.",
+        role: "Director of Sales  Tech Solutions Inc.",
+        desc: "Sales Cloud completely changed our sales process, thanks to Synexc. We struggled with manual lead tracking and disorganiz",
+    },
+    {
+        image: "/user1.png",
+        username: "Alex R.",
+        role: "Director of Sales  Tech Solutions Inc.",
+        desc: "With Service Cloud and automation from Synexc, our customer support team is thriving. Before, tickets were scattered across different channels..",
+    },
+    {
+        image: "/user1.png",
+        username: "Alex R.",
+        role: "Director of Sales  Tech Solutions Inc.",
+        desc: "We wanted to launch our app on the Salesforce AppExchange, but the security review process felt like a mountain to climb. Synexc handled ..",
+    }
+]
