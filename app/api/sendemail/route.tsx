44cd5018e6efb900a5389c731d81cca778fe29f2
sahilsimply <102518542+sahilsimply@users.noreply.github.com>
2025-03-30 06:25:26 -0400

    update code

--- app/api/sendemail/route.tsx
+++ app/api/sendemail/route.tsx
@@ -1,40 +1,49 @@
- 
 import { NextRequest, NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 
-export async function POST(request: NextRequest, response: NextResponse) {
+export async function POST(request: NextRequest) {
   try {
-    const { firstName, lastName , email,  phone, work_experience, } =
+    const { firstName, lastName, email, phone, work_experience } =
       await request.json();
 
+    // Use environment variables for security
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
       port: 587,
       secure: false,
       auth: {
-        user: "careers@rolconsultancy.com",
-        pass: "ukch asot uygi ujfd",
+        user: process.env.EMAIL_USER, // Set this in .env file
+        pass: process.env.EMAIL_PASS, // Set this in .env file
       },
     });
 
+    // Send mail to careers@rolconsultancy.com
     const info = await transporter.sendMail({
       from: email,
       to: "careers@rolconsultancy.com",
       subject: `Inquiry from RQL Consultancy`,
-
       text: "RQL Consultancy Contact Form",
-      html: `<b> Firstname: ${firstName} <br> Lastname: ${lastName} <br> Email: ${email} <br> Phone: ${phone} <br> Message: ${work_experience} }  </b>`,
+      html: `<b> First Name: ${firstName} <br> 
+                Last Name: ${lastName} <br> 
+                Email: ${email} <br> 
+                Phone: ${phone} <br> 
+                ${work_experience ? `Message: ${work_experience}` : ""} 
+            </b>`,
     });
+
+    // Send confirmation email to user
     const recipient = await transporter.sendMail({
-      from: email,
+      from: "careers@rolconsultancy.com",
       to: email,
       subject: `RQL Consultancy`,
       text: "RQL Consultancy",
-      html: `<b> We will contact you soon  </b> <br/> <b> contact no :+91 93040 53124  <br> ${work_experience ? `Message: ${work_experience}` : `University name: ${work_experience}`} </b>`,
+      html: `<b> We will contact you soon </b> <br/> 
+             <b> Contact no: +91 93040 53124 </b> <br> 
+             ${work_experience ? `Message: ${work_experience}` : ""}`,
     });
-    console.log("Message sent: %s", recipient.messageId);
 
     console.log("Message sent: %s", info.messageId);
+    console.log("Confirmation sent: %s", recipient.messageId);
 
     return NextResponse.json({
       status: 200,
