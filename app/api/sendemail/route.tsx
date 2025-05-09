a8abdf7df18bb88f014b87ce4b13ce2156b65b7e
sahilsimply <102518542+sahilsimply@users.noreply.github.com>
2025-05-09 18:28:58 +0530

    updated code

--- app/api/sendemail/route.tsx
+++ app/api/sendemail/route.tsx
@@ -6,23 +6,21 @@ export async function POST(request: NextRequest) {
     const { firstName, lastName, email, phone, work_experience } =
       await request.json();
 
-    // Use environment variables for security
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
+      host: "smtp.office365.com",
       port: 587,
-      secure: false,
+      secure: false, // Use TLS
       auth: {
-        user: "contact@synexc.com",
-        pass: "grip xobu fsva wtnh",
+        user: "contact@synexc.com", // your Microsoft 365 email
+        pass: "76%K#x8DG6vg", // see below
       },
     });
 
-    // Send mail to contact@synexc.com
+    // Internal email
     const info = await transporter.sendMail({
-      from: email,
+      from: "contact@synexc.com",
       to: "contact@synexc.com",
       subject: `Inquiry from Salesforce Consulting Company`,
-      text: "RQL Consultancy Contact Form",
       html: `<b> First Name: ${firstName} <br> 
                 Last Name: ${lastName} <br> 
                 Email: ${email} <br> 
@@ -31,26 +29,16 @@ export async function POST(request: NextRequest) {
             </b>`,
     });
 
-    // Send confirmation email to user
+    // User confirmation email
     const recipient = await transporter.sendMail({
       from: "contact@synexc.com",
       to: email,
       subject: "We’ve Received Your Request! 🚀",
-      text: `Hi,
-    
-    Thank you for reaching out to Synexc! We’ve received your details, and our sales team will be in touch with you shortly.
-    
-    If you need any assistance in the meantime, feel free to email us at contact@synexc.com or call us at +91 9810512585. We’re here to help!
-    
-    Looking forward to connecting with you.
-    
-    Best regards,
-    Team Synexc`,
       html: `<p>Hi,</p>
              <p>Thank you for reaching out to <b>Synexc</b>! We’ve received your details, and our sales team will be in touch with you shortly.</p>
              <p>If you need any assistance in the meantime, feel free to email us at <a href="mailto:contact@synexc.com">contact@synexc.com</a> or call us at <b>+91 9810512585</b>. We’re here to help!</p>
              <p>Looking forward to connecting with you.</p>
-             <p>Best regards, <br> <b>Team Synexc</b></p>`
+             <p>Best regards, <br> <b>Team Synexc</b></p>`,
     });
 
     console.log("Message sent: %s", info.messageId);
@@ -61,6 +49,7 @@ export async function POST(request: NextRequest) {
       message: "Email sent successfully",
     });
   } catch (error: any) {
+    console.error(error);
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
